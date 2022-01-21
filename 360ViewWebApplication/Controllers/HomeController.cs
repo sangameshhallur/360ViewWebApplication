6a2ae8dba543ae61130431b21cb7e1ae8d7889b1
Sangamesh <ibm.hallur@daimler.com>
2022-01-21 15:33:36 +0530

    Add project files.

---
+++ 360ViewWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace _360ViewWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult LoadOnDemandPartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Load360DegreeImageDialogPartial()
+        {
+            return PartialView("360DegreeImageDialogPartial");
+        }
+    }
+}
---
+++ 360ViewWebApplication/imagerotator/html/js/TestObject.js
@@ -0,0 +1,11 @@
+﻿function load360Image(configFileURL, graphicsPath) {
+    var rotator = WR360.ImageRotator.Create('wr360PlayerId');
+    rotator.settings.configFileURL = configFileURL;
+    /*rotator.settings.configFileURL = '../XML/f1.xml';*/
+    rotator.settings.graphicsPath = graphicsPath;
+    rotator.settings.alt = '360 degree view - f1';
+    rotator.settings.googleEventTracking = false;
+    rotator.settings.responsiveBaseWidth = 0;
+    rotator.settings.responsiveMinHeight = 0;
+    rotator.runImageRotator();
+}
